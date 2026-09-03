81ac49a0678dc84ed0f51d61246cb63b979bf0ff
KubovicsT <52443594+KubovicsT@users.noreply.github.com>
2026-09-04 00:55:36 +0300

    Install v0.9 thickness heatmap tools

--- Scripts/ExtrasInstaller.cs
+++ Scripts/ExtrasInstaller.cs
@@ -24,5 +24,6 @@ public partial class ExtrasInstaller : Node
         main.InstallV08Extras();
         main.InstallV08Hook();
         main.InstallV09Extras();
+        main.InstallV09ThicknessHeatmap();
     }
 }
