7ca345511e2a4d5599dd3d27e66ca19c527c40f5
KubovicsT <52443594+KubovicsT@users.noreply.github.com>
2026-09-03 21:59:35 +0300

    Enable v0.3 starting image extras

--- Scripts/ExtrasInstaller.cs
+++ Scripts/ExtrasInstaller.cs
@@ -14,5 +14,6 @@ public partial class ExtrasInstaller : Node
         if (GetParent() is not Main main) return;
         main.InstallV01Extras();
         main.InstallV02Extras();
+        main.InstallV03Extras();
     }
 }
